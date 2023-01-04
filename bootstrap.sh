991bf8ae0d4acec1a5c528bfdc7210b4a346c19c
Abhishek Choudhary <abhishek.choudhary@opcito.com>
2023-01-04 15:39:20 +0530

    initial setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,8 @@ export FLASK_APP=./server
 export FLASK_ENV=development
 export FLASK_DEBUG=1
 flask run -p 5001
+
+# flask-mysql = "*"
+# mysql-connector-python = "*"
+# python-dotenv = "*"
+# flask-sqlalchemy = "*" -p 5001
