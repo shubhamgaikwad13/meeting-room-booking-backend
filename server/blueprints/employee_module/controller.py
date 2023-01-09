48472580585276e2fd0405cad3db842ede8c6be2
Shubham Gaikwad <shubham.gaikwad@opcito.com>
2023-01-09 17:03:46 +0530

    adds loggers for employee and room module

--- server/blueprints/employee_module/controller.py
+++ server/blueprints/employee_module/controller.py
@@ -9,36 +9,46 @@ from .constant import *
 from mysql.connector import errorcode
 from mysql.connector import Error as MySQLError
 from ...utils import make_response
+import logging
 
 employee_bp = Blueprint('employee', __name__, url_prefix='/employee')
 
+logger = logging.getLogger('employee_module')
 
 # api for fetching all employees
 @employee_bp.route('/', methods=['GET'])
 def get_employees():
+    logger.info("Fetching all employees.")
     try:
         employees = Employee.get_employees()  # fetches employees in list
         if employees:
+            logger.info("All employees fetched successfully.")
             return make_response(employees, key="employees"), HTTPStatus.OK
 
+        logger.info(NO_EMPLOYEES_FOUND)
         return make_response(NO_EMPLOYEES_FOUND), HTTPStatus.OK
     except Exception as e:
+        logger.error(str(e))
         return make_response(FETCH_EMPLOYEED_FAILED, key="error"), HTTPStatus.BAD_REQUEST
 
 
 # api for fetching employee by id
 @employee_bp.route('/<id>', methods=['GET'])
 def get_employee_by_id(id):
+    logger.info(f"Fetching employee with id {id}")
     try:
         employee = Employee.get_employee_by_id(
             id)  # fetches employee in object
 
         if employee is None:
+            logger.info(EMPLOYEE_NOT_FOUND)
             return make_response(EMPLOYEE_NOT_FOUND), HTTPStatus.OK
 
+        logger.info("Employee fetched successfully.")
         return make_response(data=employee.__dict__, key='employee'), HTTPStatus.OK
 
     except Exception as e:
+        logger.error(str(e))
         return make_response(data=str(e), key="error"), HTTPStatus.BAD_REQUEST
 
 
@@ -46,19 +56,23 @@ def get_employee_by_id(id):
 @employee_bp.route('/', methods=['POST'])
 def add_employee():
     params = request.get_json()
-
+    logger.info("Adding a new employee.")
     try:
         # validates request parameters and throws exception for invalid params
+        logger.info("Validating add request parameters.")
         EmployeeValidation.validate(params)
 
         # checks if request is being made by admin
+        logger.info("Checking if only admin is adding an employee.")
         if not Employee.is_admin(params['created_by']):
             raise Exception(ONLY_ADMIN_ADDS_USER)
 
         employee = Employee(**params)
+        logger.info("Inserting employee record in the database.")
         employee.save()  # inserts employee record in the database
 
     except MySQLError as err:
+        logger.error(str(err))
         # Error handling for duplicate entries for phone, email and id
         if err.errno == errorcode.ER_DUP_ENTRY:
             for key in DUP_ENTRY:
@@ -69,32 +83,41 @@ def add_employee():
         return make_response(str(err), 'error'), HTTPStatus.BAD_REQUEST
 
     except Exception as e:
+        logger.error(str(e))
         return make_response(str(e), 'error'), HTTPStatus.BAD_REQUEST
 
+    logger.info(EMPLOYEE_ADDED)
     return make_response(EMPLOYEE_ADDED), HTTPStatus.OK
 
 
 # api for deleting an employee by id
 @employee_bp.route('/<id>', methods=['DELETE'])
 def delete_employee_by_id(id):
+    logger.info("Soft-Deleting an employee")
     try:
         params = request.get_json()
 
         # checks if request is being made by admin
+        logger.info("Checking if only admin is deleting an employee.")
         if not Employee.is_admin(params['updated_by']):
             raise Exception("Only admin can delete the user.")
 
         # if employee exists then deletes otherwise shows error
+        logger.info("Checking if employee exists in the database.")
         employee = Employee.get_employee_by_id(id)
 
         if employee is None:
+            logger.info(EMPLOYEE_NOT_FOUND)
             return make_response(EMPLOYEE_NOT_FOUND), HTTPStatus.BAD_REQUEST
-
+        
+        logger.info("Soft-deleting employee record in the database.")
         employee.delete()  # if employee exists then soft deletes its record
 
     except Exception as e:
+        logger.error(str(e))
         return make_response(str(e), 'error'), HTTPStatus.BAD_REQUEST
 
+    logger.info("Employee deleted succesfully.")
     return make_response(EMPLOYEE_DELETED), HTTPStatus.OK
 
 
@@ -102,23 +125,29 @@ def delete_employee_by_id(id):
 @employee_bp.route('/<id>', methods=['PATCH'])
 def update_employee(id):
     params = request.get_json()
+    logger.info(f"Updating an employee {id}")
 
     try:
         EmployeeValidation.validate_update_request(params)
 
         # checks if request is being made by admin
+        logger.info("Checking if only admin is updating an employee.")
         if not Employee.is_admin(params['updated_by']):
             raise Exception(ONLY_ADMIN_ADDS_USER)
         
         # if employee exists then updates otherwise shows error
+        logger.info("Checking if employee exists in the database.")
         employee = Employee.get_employee_by_id(id)
         if employee is None:
+            logger.info(EMPLOYEE_NOT_FOUND)
             return make_response(EMPLOYEE_NOT_FOUND), HTTPStatus.BAD_REQUEST
 
         employee.update(params)
+        logger.info("Employee updated successfully.")
         return make_response("Employee record updated successfully."), HTTPStatus.OK
 
     except Exception as e:
+        logger.error(str(e))
         return make_response(str(e), 'error'), HTTPStatus.BAD_REQUEST
     
 
--- server/blueprints/room_module/controller.py
+++ server/blueprints/room_module/controller.py
@@ -8,26 +8,32 @@ from mysql.connector import errorcode
 from mysql.connector import Error as MySQLError
 from ...utils import make_response, dup_message
 from ..employee_module.EmployeeDAO import Employee
+import logging
 
 room_bp = Blueprint('room', __name__, url_prefix='/room')
-
+logger = logging.getLogger("room_module")
 
 @room_bp.route('/', methods=['POST'])
 def add_room():
     params = request.get_json()
+    logger.info("Adding a new room.")
 
     try:
         # validates request parameters and throws exception for invalid params
+        logger.info("Validating add request parameters.")
         RoomValidation.validate(params)
 
         # checks if request is being made by admin
+        logger.info("Checking if only admin is adding the room.")
         if not Employee.is_admin(params['created_by']):
             raise Exception(ONLY_ADMIN_ADDS_ROOM)
 
         room = Room(**params)
+        logger.info("Inserting room record in the database.")
         room.save()  # inserts room record in the database
 
     except MySQLError as err:
+        logger.error(str(err))
         # Error handling for duplicate entries for title
         if err.errno == errorcode.ER_DUP_ENTRY:
             for key in DUP_ENTRY:
@@ -38,59 +44,76 @@ def add_room():
         return make_response(str(err), 'error'), HTTPStatus.BAD_REQUEST
 
     except Exception as e:
+        logger.error(str(e))
         return make_response(str(e), 'error'), HTTPStatus.BAD_REQUEST
 
+    logger.info(ROOM_ADDED)
     return make_response(ROOM_ADDED), HTTPStatus.OK
 
 
 
 @room_bp.route('/', methods=['GET'])
 def get_rooms():
+    logger.info("Fetching all rooms.")
     try:
         rooms = Room.get_rooms()  # fetches rooms in list
         if rooms:
+            logger.info("All rooms fetched successfully.")
             return make_response(rooms, key="rooms"), HTTPStatus.OK
 
+        logger.info(NO_ROOMS_FOUND)
         return make_response(NO_ROOMS_FOUND), HTTPStatus.OK
     except Exception as e:
+        logger.error(str(e))
         return make_response(FETCH_ROOMS_FAILED, key="error"), HTTPStatus.BAD_REQUEST
 
 
 
 @room_bp.route('/<id>', methods=['GET'])
 def get_room_by_id(id):
+    logger.info(f"Fetching room with id {id}")
     try:
         room = Room.get_room_by_id(id)  # fetches room in object
 
         if room is None:
+            logger.info(ROOM_NOT_FOUND)
             return make_response(ROOM_NOT_FOUND), HTTPStatus.OK
 
+        logger.info("Room fetched successfully.")
         return make_response(data=room, key='room'), HTTPStatus.OK
 
     except Exception as e:
+        logger.error(str(e))
         return make_response(data=str(e), key="error"), HTTPStatus.BAD_REQUEST
 
 
 @room_bp.route('/<id>', methods=['DELETE'])
 def delete_room_by_id(id):
+    logger.info("Deleting the room.")
     try:
         params = request.get_json()
 
         # checks if request is being made by admin
+        logger.info("Checking if only admin is deleting the room.")
         if not Employee.is_admin(params['updated_by']):
             raise Exception("Only admin can delete the room.")
 
         # if room exists then deletes otherwise shows error
+        logger.info("Checking if room exists in the database.")
         room = Room.get_room_by_id(id)
 
         if room is None:
+            logger.info(ROOM_NOT_FOUND)
             return make_response(ROOM_NOT_FOUND), HTTPStatus.BAD_REQUEST
-
+        
+        logger.info("Deleting room record in the database.")
         room.delete()  # if room exists then deletes its record
 
     except Exception as e:
+        logger.error(str(e))
         return make_response(str(e), 'error'), HTTPStatus.BAD_REQUEST
 
+    logger.info("Room deleted succesfully.")
     return make_response(ROOM_DELETED), HTTPStatus.OK
 
 
@@ -98,21 +121,27 @@ def delete_room_by_id(id):
 @room_bp.route('/<id>', methods=['PATCH'])
 def update_room_by_id(id):
     params = request.get_json()
+    logger.info(f"Updating an room {id}")
 
     try:
         RoomValidation.validate_update_request(params)
 
         # checks if request is being made by admin
+        logger.info("Checking if only admin is updating the room.")
         if not Employee.is_admin(params['updated_by']):
             raise Exception(ONLY_ADMIN_UPDATES_ROOM)
         
         # if room exists then updates otherwise shows error
+        logger.info("Checking if room exists in the database.")
         room = Room.get_room_by_id(id)
         if room is None:
+            logger.info(ROOM_NOT_FOUND)
             return make_response(ROOM_NOT_FOUND), HTTPStatus.BAD_REQUEST
 
         room.update(params)
+        logger.info("Room updated successfully.")
         return make_response("Room record updated successfully."), HTTPStatus.OK
 
     except Exception as e:
+        logger.error(str(e))
         return make_response(str(e), 'error'), HTTPStatus.BAD_REQUEST
